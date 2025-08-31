8b1611ad777e4683272d958de8958e3fd2fa1bfd
Yakir Adela <yakirade@gmail.com>
2025-08-31 15:53:05 +0300

    Update main.tf

--- infra/modules/eks/main.tf
+++ infra/modules/eks/main.tf
@@ -1,15 +1,15 @@
 module "eks_cluster" {
-    source                  = "terraform-aws-modules/eks/aws"
-    cluster_name            = "${var.env}-eks"
-    cluster_version         = "1.29"
-    vpc_id                  = var.vpc_id
-    subnet_ids              = var.private_subnets 
+  source          = "terraform-aws-modules/eks/aws"
+  cluster_name    = "dev-eks"
+  cluster_version = "1.29"
+  vpc_id          = "vpc-0123456789abcdef0"
+  subnet_ids      = ["subnet-abc123", "subnet-def456"]
 
-    manage_node_groups = {
-        default = {
-            instance_types  = ["m5.large"]
-            min_size        = 2
-            max_size        = 5
-        }
+  manage_node_groups = {
+    default = {
+      instance_types = ["m5.large"]
+      min_size       = 2
+      max_size       = 5
     }
+  }
 }
