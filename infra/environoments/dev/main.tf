18025e88a42dad35ebf865871ddfdc32cc595e0d
Yakir Adela <yakirade@gmail.com>
2025-08-31 15:52:05 +0300

    Update main.tf

--- infra/environoments/dev/main.tf
+++ infra/environoments/dev/main.tf
@@ -3,54 +3,44 @@ terraform {
 }
 
 provider "aws" {
-  region = var.region
+  region = "us-east-2"
 }
 
 module "vpc" {
-  source               = "../../modules/vpc"
-  cidr_block           = "10.10.0.0/16"
-  env                  = "dev"
-  private_subnet_cidrs = ["10.10.1.0/24", "10.10.2.0/24"]
-  azs                  = ["us-east-2a", "us-east-2b"]
+  source = "../../modules/vpc"
 }
 
 module "eks" {
   source          = "../../modules/eks"
-  env             = "dev"
-  vpc_id          = module.vpc.vpc_id
-  private_subnets = module.vpc.private_subnets
+  vpc_id          = "vpc-0123456789abcdef0"
+  private_subnets = ["subnet-abc123", "subnet-def456"]
 }
 
 module "rds" {
   source          = "../../modules/rds"
-  env             = "dev"
-  private_subnets = module.vpc.private_subnets
-  sg_id           = module.vpc.db_sg_id
+  private_subnets = ["subnet-abc123", "subnet-def456"]
+  sg_id           = "sg-0123456789abcdef0"
   username        = "etl"
   password        = "chance-me"
 }
 
 module "kafka" {
   source          = "../../modules/kafka"
-  env             = "dev"
-  private_subnets = module.vpc.private_subnets
-  sg_id           = module.vpc.app_sg_id
+  private_subnets = ["subnet-abc123", "subnet-def456"]
+  sg_id           = "sg-0fedcba9876543210"
 }
-
 
 module "s3" {
-  source = "../../modules/s3"
-  bucket_name = "dev-data-platform-bucket"
-  force_destroy = true
-  versioning_enabled = true
+  source              = "../../modules/s3"
+  bucket_name         = "dev-data-platform-bucket"
+  force_destroy       = true
+  versioning_enabled  = true
 }
 
-
 module "redshift" {
-  source                     = "../../modules/redshift"
-  env                        = "dev"
-  subnet_ids                 = module.vpc.private_subnets
-  master_username            = "adminuser"
-  master_password            = "yakiradela@A"
-  redshift_database_name     = "dataplatform"
+  source                  = "../../modules/redshift"
+  subnet_ids              = ["subnet-abc123", "subnet-def456"]
+  master_username         = "adminuser"
+  master_password         = "yakiradela@A"
+  redshift_database_name  = "dataplatform"
 }
