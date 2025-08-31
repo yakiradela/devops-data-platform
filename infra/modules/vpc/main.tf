117352b2a2a5ea8c7f0ddc6f5c728cf6d823193e
Yakir Adela <yakirade@gmail.com>
2025-08-31 15:52:41 +0300

    Update main.tf

--- infra/modules/vpc/main.tf
+++ infra/modules/vpc/main.tf
@@ -1,20 +1,29 @@
 resource "aws_vpc" "main" {
-    cidr_block            = var.cidr_block
-    enable_dns_support    = true 
-    enable_dns_hostnames  = true 
+  cidr_block           = "10.10.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-    tags = {
-      Name                = "${var.env}-vpc"
-    }
+  tags = {
+    Name = "dev-vpc"
+  }
 }
 
-resource "aws_subnet" "private" {
-    count                 = length(var.private_subnet_cidrs)
-    vpc_id                = aws_vpc.main.id 
-    cidr_block            = element(var.private_subnet_cidrs, count.index)
-    availability_zone     = element(var.azs, count.index)
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.1.0/24"
+  availability_zone = "us-east-2a"
 
-    tags = {
-      Name                = "${var.env}-private-${count.index}"
-    }
+  tags = {
+    Name = "dev-private-a"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.10.2.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "dev-private-b"
+  }
 }
